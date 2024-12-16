16afae454aeaa2704ea2754cb2bb954a6093aa0b
NoamHack <noam8200@gmail.com>
2024-12-16 10:41:04 +0200

    :sparkles: feat: Add nav-bar component

---
+++ apps/Frontend/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { AuthGuard } from '../auth/guard/auth.guard';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss'],
+})
+export class NavBarComponent {
+  items: MenuItem[];
+
+  constructor(private router: Router) {
+    this.items = [
+      {
+        label: 'Add Solider',
+        icon: 'pi pi-user-plus',
+        command: () => {
+          AuthGuard.AccessUrlNavigation();
+          this.router.navigate(['/add'], {
+            skipLocationChange: true,
+          });
+        },
+      },
+      {
+        label: 'Sign-In',
+        icon: 'pi pi pi-sign-in',
+        command: () => {
+          AuthGuard.AccessUrlNavigation();
+          this.router.navigate(['/login'], {
+            skipLocationChange: true,
+          });
+        },
+      },
+      {
+        label: 'Video',
+        icon: 'pi pi-video',
+        command: () => {
+          AuthGuard.AccessUrlNavigation();
+          this.router.navigate(['/video'], {
+            skipLocationChange: true,
+          });
+        },
+      },
+    ];
+  }
+}
