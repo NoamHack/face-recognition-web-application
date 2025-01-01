082fce1225fe6285b08a39efd15a46b5522ae60b
Noam Levy <noam8200@gmail.com>
2025-01-01 16:14:40 +0200

    ➕ add: Add and organize negative images

--- apps/image-processing/image_processing/index.py
+++ apps/image-processing/image_processing/index.py
@@ -7,6 +7,13 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Layer, Conv2D, Dense, MaxPooling2D, Input, Flatten
 import tensorflow as tf
 
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
   tf.config.experimental.set_memory_growth(gpu, True)
+
+POS_PATH = os.path.join('data', 'positive')
+NEG_PATH = os.path.join('data', 'negative')
+ANC_PATH = os.path.join('data', 'anchor')
+
